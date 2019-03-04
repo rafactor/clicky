e62b948e80a60aef5ff591e27e81a747dff864f6
rafactor <avila.rafael@gmail.com>
2019-03-04 07:26:22 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Header from "./Components/Header"
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Header>
+
+       </Header>
+      </div>
+    );
+  }
+}
+
+export default App;
