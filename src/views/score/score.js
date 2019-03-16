6963c3e8bf978e5fb7569b620592c3a1e522c8a1
rafactor <avila.rafael@gmail.com>
2019-03-16 10:24:53 -0400

    fix build

--- src/views/score/score.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class Score extends Component {
-    render() {
-        return (
-            <div>This is the Scores Page</div>
-        )
-    }
-}
